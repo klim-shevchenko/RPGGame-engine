ca8e81f6ae887655a462e35da17eab40300a013a
klim-shevchenko <147738915+klim-shevchenko@users.noreply.github.com>
2024-05-01 07:55:02 +0300

    Update main.py
    изменение файла main вручную, потому что не могу сделать git push из кода, возникают, какие-то ошибки

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
-from rpggame import *
+from rpg.Graphics import *
 
 root = tk.Tk()
-root.geometry('400x400')
+root.geometry('1500x1500')
 
 # Создание экземпляра класса Graphics, который будет взаимодействовать с окном
 canvas = Graphics(root, width=1500, height=1500)
-canvas.pack()
 
 # Загрузка изображения
-image1 = tk.PhotoImage(file='C:/Users/KShevchenko/PycharmProjects/RPGGame-engine/images/fon1.png')
-image2 = tk.PhotoImage(file='C:/Users/KShevchenko/PycharmProjects/RPGGame-engine/images/person1.png')
+im1 = Sprite(image='images/fon1.png')
+im2 = Sprite(image='images/person1.png')
 
 # Добавление изображения на Canvas с помощью метода add_sprite
-tag1 = canvas.add_sprite(image1, 100, 100)
-tag2 = canvas.add_sprite(image2, 150, 100)
-#tag2 = canvas.add_sprite(image2, image=image2, subset =" 100 100 200 200")
+tag1 = canvas.add_sprite(im1.spr_image, 140, 140)
+tag2 = canvas.add_sprite(im2.spr_image, 200, 100)
+
+# Функции, которые будут вызываться при нажатии кнопки
+def on_button_click():
+    canvas.change_sprite(tag1, im2.spr_image)
+def on_button_click1():
+    canvas.delete_sprite(tag1)
+def on_button_click2():
+    canvas.update(tag2, 210, 100)
+def on_button_click3():
+    canvas.clear_all()
+
+# Создание кнопки
+button = tk.Button(root, text="поменять спрайт", command=on_button_click)
+button1 = tk.Button(root, text="удалить спрайт", command=on_button_click1)
+button2 = tk.Button(root, text="передвинуть спрайт", command=on_button_click2)
+button3 = tk.Button(root, text="очистить всё", command=on_button_click3)
+
+# Размещение кнопки на окне
+button.pack()
+button1.pack()
+button2.pack()
+button3.pack()
+canvas.pack()
 # Основной цикл обработки событий
 root.mainloop()
