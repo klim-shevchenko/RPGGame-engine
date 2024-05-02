26eb1fad7c774cc44f0d111f65885d219a418ebd
klim-shevchenko <147738915+klim-shevchenko@users.noreply.github.com>
2024-05-02 22:40:21 +0300

    Update Graphics.py
    обновил Canvas

--- rpg/Graphics.py
+++ rpg/Graphics.py
@@ -1,24 +1,33 @@
 import tkinter as tk
+#from Sprite import Sprite
 class Graphics(tk.Canvas):
+    """Класс Canvas с дополнительными методами для работы со спрайтами."""
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
+        self.sprites = []  # список спрайтов
 
-    def add_sprite(self, item, x, y, **kwargs):
-        tag = self.create_image(x, y, image=item, anchor='center',  **kwargs)
-        return tag
+    def add_sprite(self, sprite, x, y, **kwargs):
+        """Добавляет спрайт на Canvas."""
+        tag = self.create_image(x, y, image=sprite.image, anchor='center', **kwargs)
+        sprite.set_tag(tag)
+        self.sprites.append(sprite)
     # anchor='center'
 
-    def change_sprite(self, tag, new_image):
-        self.itemconfig(tag, image=new_image)
+    def change_sprite(self, sprite, new_image):
+        """Изменяет изображение спрайта."""
+        self.itemconfig(sprite.get_tag(), image=new_image)
 
-    def delete_sprite(self, tag):
-        self.delete(tag)
+    def delete_sprite(self, sprite):
+        """Удаляет спрайт с Canvas."""
+        self.delete(sprite.get_tag())
+        self.sprites.remove(sprite)
 
     def clear_all(self):
-        self.delete("all")
+        """Удаляет все спрайты с Canvas."""
+        for sprite in self.sprites:
+            self.delete(sprite.get_tag())
+        self.sprites.clear()
 
-    def update(self, tag, new_x, new_y):
-        self.coords(tag, new_x, new_y)
-class Sprite():
-    def __init__(self, image):
-        self.spr_image = tk.PhotoImage(file=image)
+    def update(self, sprite, new_x, new_y):
+        """Обновляет координаты спрайта на Canvas."""
+        sprite.update(new_x, new_y)
